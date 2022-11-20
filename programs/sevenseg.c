da2ddc7b2c62406d0e82982ef5255342f99dbd1e
Lucas Merritt <lucas.j.merritt@gmail.com>
2022-11-20 00:49:16 -0700

    code refactoring && moved hardware around

--- programs/sevenseg.c
+++ programs/sevenseg.c
@@ -5,90 +5,79 @@
 
 
 /*Segment	Port	Pin*/
+//D1		B		0
+//D2		B		1
+//D3		B		2
+//D4		B		3
+//Decimal	C		0
+//G			C		1
 //A			D		2
 //B			D		3
 //C			D		4
 //D			D		5
 //E			D		6
 //F			D		7
-//G			B		0
-//D1		B		1
-//D2		B		2
-//D3		B		3
-//D4		B		4
-//Decimal	A		5
 
 #include <util/delay.h>
 
-#define PORTD_DATA (*((volatile unsigned char *)0x2B))
 #define PORTB_DATA (*((volatile unsigned char *)0x25))
-#define PORTD_DDR (*((volatile unsigned char *)0x2A))
+#define PORTC_DATA (*((volatile unsigned char *)0x28))
+#define PORTD_DATA (*((volatile unsigned char *)0x2B))
 #define PORTB_DDR (*((volatile unsigned char *)0x24))
+#define PORTC_DDR (*((volatile unsigned char *)0x27))
+#define PORTD_DDR (*((volatile unsigned char *)0x2A))
 
-struct displayDigit {
+//Struct for displayable characters
+struct displayChar {
 	unsigned char displayedSegments;
 	unsigned char gSegment;
+};
+
+//All displayable digits
+struct displayChar displayed[10] = {
+{0xFC, 0},//0
+{0x18, 0},//1
+{0x6C, 1},//2
+{0x3C, 1},//3
+{0x96, 1},//4
+{0xB4, 1},//5
+{0xF4, 1},//6
+{0x1C, 0},//7
+{0xFC, 1},//8
+{0x9C, 1},//9
 };
 
 char charD[] = "0123";
 
 int main() {
 	//Set directions(All output)
+	PORTB_DDR |= 0xF;
+	PORTC_DDR |= 0x3;
 	PORTD_DDR |= 0xFC;
-	PORTB_DDR |= 0x1F;
-	//All displayable digits
-	struct displayDigit displayed[10] = {
-	{0xFC, 0},//0
-	{0x18, 0},//1
-	{0x6C, 1},//2
-	{0x3C, 1},//3
-	{0x96, 1},//4
-	{0xB4, 1},//5
-	{0xF4, 1},//6
-	{0x1C, 0},//7
-	{0xFC, 1},//8
-	{0x9C, 1},//9
-	};
-	/*struct displayDigit nums[4];
-	nums[0] = displayed[0];
-	nums[1] = displayed[1];
-	nums[2] = displayed[2];
-	nums[3] = displayed[3];
-	for(char i=0; i<4; i++) {
-		nums[i] = displayed[(int)charD[i]];
-	}*/
 	//Create array(TODO: make nums based off of charD)
-	struct displayDigit nums[4] = {
+	struct displayChar nums[4] = {
 	{0xFC, 0},//0
 	{0x18, 0},//1
 	{0x6C, 1},//2
 	{0x3C, 1},//3
 	};
 	//Display index
-	char iL = 1;
+	char iL = 0;
 
 	while(1<2) {
 		//Clear display
+		PORTB_DATA &= ~0xF;
+		PORTC_DATA &= ~0x3;
 		PORTD_DATA &= ~0xFC;
-		PORTB_DATA &= ~0x1F;
 		//Set display number
-		PORTB_DATA |= ((1<<iL)^0x1E);
-		//Set G segment
-		//PORTB_DATA |= nums[iL-1].gSegment;
+		PORTB_DATA |= 0xF-(1<<iL);
 		//Set the rest of the segments
-		//PORTD_DATA |= nums[iL-1].displayedSegments;
+		//PORTD_DATA |= nums[iL].displayedSegments;
 		PORTD_DATA |= 0xFC;
-		//Check if index is maximum
-		if(iL==4) {
-			//Reset index
-			iL = 1;
-		}
-		else {
-			//Incrment index
-			iL++;
-		}
+		//Set index appropiately
+		iL += (iL==3 ? -3 : 1);
 		//Wait(TODO: homemade delay function)
-		//1s used for timing 1ms used for production
+		//1s used for debugging 1ms used for production
 		_delay_ms(1000);
 	}
 }
