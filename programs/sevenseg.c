24ccfdcd0d3825d2fc8110e38ad09cadaad5695a
Lucas Merritt <lucas.j.merritt@gmail.com>
2022-11-23 09:49:36 -0700

    added somewhat functional timer

--- programs/sevenseg.c
+++ programs/sevenseg.c
@@ -27,19 +27,6 @@
 #define PORTC_DDR (*((volatile unsigned char *)0x27))
 #define PORTD_DDR (*((volatile unsigned char *)0x2A))
 
-//All displayable digits
-unsigned char displayed[10][2] = {
-{0xFC, 0},//0
-{0x18, 0},//1
-{0x6C, 1},//2
-{0x3C, 1},//3
-{0x96, 1},//4
-{0xB4, 1},//5
-{0xF4, 1},//6
-{0x1C, 0},//7
-{0xFC, 1},//8
-{0x9C, 1},//9
-};
 
 //...
 //if I even change the name of this it screws everything up
@@ -52,23 +39,36 @@ int main() {
 	PORTB_DDR |= 0xF;
 	PORTC_DDR |= 0x3;
 	PORTD_DDR |= 0xFC;
+    //All displayable digits
+    unsigned char displayed[10][2] = {
+        {0xFC, 0},//0
+        {0x18, 0},//1
+        {0x6C, 1},//2
+        {0x3C, 1},//3
+        {0x98, 1},//4
+        {0xB4, 1},//5
+        {0xF4, 1},//6
+        {0x1C, 0},//7
+        {0xFC, 1},//8
+        {0x9C, 1},//9
+    };
 	//Create array
 	unsigned char nums[4][2] = {
-	{0xFC, 0},//0
-	{0xFC, 0},//0
-	{0xFC, 0},//0
-	{0xFC, 0},//0
+	    {0xFC, 0},//0
+	    {0xFC, 0},//0
+	    {0xFC, 0},//0
+	    {0xFC, 0},//0
 	};
 	//Display index
-	unsigned char iL = 0;
+	int iL = 0;
 	//Time
-	long timeMs = 00.00;
-	//Precision or extra time switch: TODO: implement on hardware
-	unsigned char precision = 1;
+	int timeMs = 0000;
+	//Precision/extra time switch: TODO: implement on hardware
+	int precision = 1;
 	//Digit update rate(time before switching to different display), ms
-	double delayT = 1;
+	int delayT = 1;
 	//Time update index
-	unsigned char iT;
+	int iT;
     //Set iT accordingly
     if(precision==1){iT=10;}
     else{iT=100;}
@@ -90,10 +90,32 @@ int main() {
 		PORTC_DATA |= (setG*2);
 		//Set decimal point(depends on precision switch)
 		if(iL==(2-precision)){PORTC_DATA|=1;};
-		//Set indexes appropriately
+		//Set index appropriately
 		iL += (iL==3 ? -3 : 1);
 		//Wait(TODO: homemade delay function, maybe?)
 		_delay_ms(delayT);
+        //Decrement time update index and check if 0
+        if(iT!=0){iT--;}
+        else {
+            //Reset index
+            if(precision==1){iT=10;}
+            else{iT=100;}
+            //Update time
+            timeMs++;
+            //Create temp version of time to get individual digits
+            int tempTimeMs = timeMs;
+            for(int i=4; i!=0; i--) {
+                //Get last digit in number
+                int digit = tempTimeMs % 10;
+                //Temp while I try to fix code
+                digit = 1;
+                //Essentially remove the last digit for next iteration
+                tempTimeMs /= 10;
+                //Get details for digit and set nums to it
+                nums[i-1][0] = displayed[digit][0];
+                nums[i-1][1] = displayed[digit][1];
+            }
+        }
 	}
 }
 
